33c3a3e0ee778937f49b3d5eefec641f9337fc9f
Thomas <thomas.hormes@hotmail.de>
2020-02-27 14:20:14 +0100

    Added GracefulShutdown

--- src/main/java/com/asys1920/ordermanagement/OrderManagementApplication.java
+++ src/main/java/com/asys1920/ordermanagement/OrderManagementApplication.java
@@ -1,9 +1,13 @@
 package com.asys1920.ordermanagement;
 
+import com.asys1920.util.GracefulShutdown;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @EntityScan("com.asys1920.*")
@@ -13,5 +17,17 @@ public class OrderManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagementApplication.class, args);
+	}
+
+	@Bean
+	public GracefulShutdown gracefulShutdown() {
+		return new GracefulShutdown();
+	}
+
+	@Bean
+	public ConfigurableServletWebServerFactory webServerFactory(final GracefulShutdown gracefulShutdown) {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.addConnectorCustomizers(gracefulShutdown);
+		return factory;
 	}
 }
